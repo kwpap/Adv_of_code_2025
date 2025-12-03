35cb2bfe31bfd3472f190f6de45f190eac27e45c
Kostantinos Papadopoulos <56506029+kwpap@users.noreply.github.com>
2025-12-03 12:17:52 +0000

    day 2

--- Day01/solution.c
+++ Day01/solution.c
@@ -17,8 +17,10 @@ int main(void) {
     int count = 0;
     int count_of_second_task = 0;
     int remeinder = 50;
+    int last_count = 0;
     // Read and process input
-    while (fgets(line, MAX_LINE_LENGTH, fp) != NULL) {
+    while (fgets(line, MAX_LINE_LENGTH, fp) != NULL ) {
+        last_count = count_of_second_task;
         // Remove newline character
         line[strcspn(line, "\n")] = 0;
         
@@ -26,24 +28,26 @@ int main(void) {
         char direction = line[0];
         int value = atoi(&line[1]);
         if (direction == 'R') {
-
-            while (remeinder + value >= 100) {
+            if (remeinder + value >= 100) {
                 count_of_second_task++;
-                value -= 100;
             }
-            remeinder = (remeinder + value) % 100;
+            remeinder = (remeinder + value) %100;
 
         } else {
-            while (remeinder - value < 0) {
+            if (remeinder - value <= 0) {
                 count_of_second_task++;
-                value -= 100;
             }
-            remeinder = (remeinder - value + 100) % 100;
+            remeinder = (remeinder - value ) % 100;
         }
         if (remeinder == 0) {
             count++;
-            count_of_second_task ++;
         }
+        // if (last_count - count_of_second_task > 1) {
+
+        //     printf("Direction: %c, Value: %d, Remeinder: %d, Count: %d, Count of second task: %d\n", direction, value, remeinder, count, count_of_second_task);
+
+        // }
+        //printf("Direction: %c, Value: %d, Remeinder: %d, Count: %d, Count of second task: %d\n", direction, value, remeinder, count, count_of_second_task);
     }
 
     fclose(fp);
